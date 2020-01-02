a5015c1b1bf2d392ca67d42d629ba59acf84c118
hyun <coderhyun476@gmail.com>
2020-01-02 12:32:41 +0900

    플레이스토어에 첫업로드

---
+++ Assets/Scripts/DataBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static DataBase DB = null;
+    public GameObject[] models;
+    public string[] names;
+    public string[] descriptions;
+
+    public bool rotateHorizontal;
+    public bool rotateVertical;
+    public bool zoomAble;
+    public bool rotateFix;
+    public float fixSmooth;
+    public float fixDelay;
+
+    void Awake()
+    {
+        if (DB == null)
+        {
+            DB = this;
+        }
+        else if (DB != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public GameObject Get3DModel(string key)
+    {
+        return models[key2index(key)];
+    }
+    public string GetDescription(string key)
+    {
+        return descriptions[key2index(key)];
+    }
+
+
+    private int key2index(string key)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (names[i] == key) return i;
+        }
+        return 0;
+    }
+}
---
+++ Assets/Scripts/TrackedImageInfoManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageInfoManager : MonoBehaviour
+{
+    private ARTrackedImageManager m_TrackedImageManager;
+
+
+    void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            // trackedImage.destroyOnRemoval = true;
+            Debug.Log(trackedImage.referenceImage.name);
+            GameObject instante = Instantiate(DataBase.DB.Get3DModel(trackedImage.referenceImage.name), trackedImage.transform) as GameObject;
+            instante.AddComponent<ARObjRotation>();
+        }
+    }
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
+
+class ARObjRotation : MonoBehaviour
+{
+
+    private Quaternion myRot;
+    private float rotX;
+    private float rotY;
+    private bool isDown = true;
+    private bool isX = false;
+    private bool isY = false;
+    private bool isFix = false;
+    private float rotSpeed = 250;
+    private float fixDelay = 2.0f;
+    private float fixSmooth = 150;
+
+    private void Awake()
+    {
+        //myRot지정
+        myRot = transform.rotation;
+        //DataBase와 연동
+        isX = DataBase.DB.rotateHorizontal;
+        isY = DataBase.DB.rotateVertical;
+        isFix = DataBase.DB.rotateFix;
+        fixSmooth = DataBase.DB.fixSmooth;
+        fixDelay = DataBase.DB.fixDelay;
+        Debug.Log(isX);
+        Debug.Log(isY);
+        Debug.Log(isFix);
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("Drag");
+        isDown = true;
+        StopAllCoroutines();
+
+        if (isX)
+        {
+            rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+            transform.Rotate(Vector3.up, -rotX, Space.Self);
+        }
+        if (isY)
+        {
+            rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+            transform.Rotate(Vector3.right, rotY, Space.Self);
+        }
+        if (isFix)
+        {
+            StartCoroutine(FixRot());
+        }
+    }
+    IEnumerator FixRot()
+    {
+        yield return new WaitForSeconds(fixDelay);
+        isDown = false;
+    }
+    private void Update()
+    {
+        if (!isDown)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, myRot, fixSmooth * Time.deltaTime);
+        }
+    }
+}
